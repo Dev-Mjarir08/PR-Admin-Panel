ae40c15d523c4a6d4ad60699389c964f5fd08912
Dev-Mjarir08 <08mjarir@gmail.com>
2026-03-31 22:29:06 +0530

    pr-admin-panel

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 import userRouter from "./user.route.js";
 import productRouter from "./product.route.js";
 import catRouter from "./catagory.route.js";
-import catSubRouter from "./subCatagory.route.js";
+import catSubRouter from "./subcategory.route.js";
 import extracatRouter from "./extraCatagory.route.js";
 
 const apiRouter = Router();
